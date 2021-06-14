0f40dcc02226e85cedea3e815952dc5d88052fb6
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-06-14 13:34:07 +0900

    9. seed_rooms

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,7 +1,9 @@
+import random
 from os import name
 from django.core.management.base import BaseCommand
 from django_seed import Seed
-from rooms.models import Room
+from users import models as user_models
+from rooms import models as room_models
 
 
 class Command(BaseCommand):
@@ -16,9 +18,21 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         number = options.get("number")
         seeder = Seed.seeder()
+        all_users = user_models.User.objects.all()
+        room_type = room_models.RoomType.objects.all()
         seeder.add_entity(
-            Room,
+            room_models.Room,
             number,
+            {
+                "name": lambda x: seeder.faker.address(),
+                "host": lambda x: random.choice(all_users),
+                "room_type": lambda x: random.choice(room_type),
+                "price": lambda x: random.randint(0, 10000),
+                "guests": lambda x: random.randint(1, 10),
+                "beds": lambda x: random.randint(1, 5),
+                "bedrooms": lambda x: random.randint(1, 5),
+                "baths": lambda x: random.randint(1, 5),
+            },
         )
         seeder.execute()
         self.stdout.write(self.style.SUCCESS(f"{number} Rooms created!"))
