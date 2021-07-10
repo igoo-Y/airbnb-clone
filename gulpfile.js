40ab83cbae770508c3c238687350e8845fa2ac84
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-10 19:23:00 +0900

    tailwindCSS set up

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ const gulp = require("gulp");
 
 const css = () => {
   const postCSS = require("gulp-postcss");
-  const sass = require('gulp-sass');
+  const sass = require("gulp-sass")(require("node-sass"));
   const minify = require("gulp-csso");
   sass.compiler = require("node-sass");
   return gulp
-    .src("assets/scss/styles.scss")
+    .src("assets/scss/styles.scss", {allowEmpty: true})
     .pipe(sass().on("error", sass.logError))
     .pipe(postCSS([require("tailwindcss"), require("autoprefixer")]))
     .pipe(minify())
