b57441ae5b708b784e6d00155e12c180df00bbfb
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-12 11:01:10 +0900

    CSS Room Card

--- rooms/models.py
+++ rooms/models.py
@@ -120,3 +120,7 @@ class Room(core_models.TimeStampedModel):
             return round(result, 2)
         else:
             return 0
+
+    def first_photo(self):
+        (photo,) = self.photos.all()[:1]
+        return photo.file.url
