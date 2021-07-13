cb224833d25af30da18d22c9873f78d1dc3a6e33
Ingyu Yang <79055280+igoo-Y@users.noreply.github.com>
2021-07-13 21:28:48 +0900

    Update Profile

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ import requests
 from requests.sessions import Request
 from users import models
 from django.urls import reverse_lazy
-from django.views.generic import FormView, DetailView
+from django.views.generic import FormView, DetailView, UpdateView
 from django.shortcuts import redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls.base import reverse
@@ -200,3 +200,22 @@ class UserProfileView(DetailView):
 
     model = models.User
     context_object_name = "user_obj"
+
+
+class UpdateProfileView(UpdateView):
+
+    model = models.User
+    template_name = "users/update_profile.html"
+    fields = (
+        "first_name",
+        "last_name",
+        "avatar",
+        "gender",
+        "bio",
+        "birthdate",
+        "language",
+        "currency",
+    )
+
+    def get_object(self, queryset=None):
+        return self.request.user
